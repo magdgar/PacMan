c19a7a7de61eb1074b23d82dbce7fd035b9cbc69
magdgar <magdgar@gmail.com>
2015-04-28 22:37:32 +0200

    discrete Pacman movement

--- objects/pacman.py
+++ objects/pacman.py
@@ -5,13 +5,13 @@ class PacMan:
     def __init__(self, x, y):
         self.x = x              # x position in the matrix
         self.y = y              # y position in the matrix
-        self.speed = 5
+        self.speed = 4/9
         self.animations = media.sprites.PacManAnim
         for key, animation in self.animations.items():
             animation.play()
 
     def paint(self, window_surface, direction):
-        self.animations[direction].blit(window_surface, (self.x * 20, self.y * 20))  # each background png is 20x20
+        self.animations[direction].blit(window_surface, (int(self.x) * 20, int(self.y) * 20))  # each background png is 20x20
 
     def move(self, dx, dy):
         self.x += dx
