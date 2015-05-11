6a752453f4e82f39dea465c1c441fbda8beaaaec
magdgar <magdgar@gmail.com>
2015-05-11 22:11:50 +0200

    naprawa napsucia:)

--- gameloop/gameloop.py
+++ gameloop/gameloop.py
@@ -1,6 +1,4 @@
 import painter.painter
-from media.dirtyrect import *
-from game_engine.game_engine import *
 
 
 class GameLoop:
@@ -9,6 +7,5 @@ class GameLoop:
         self.painter = painter.painter.Painter(window_surface)
 
     def perform_one_cycle(self, events):
-        add_dirty_rect(Rect(0, 0, 40, 40))
-        self.painter.paint_objects()
         self.game_engine.simulate_world(events)
+        self.painter.paint_objects()
