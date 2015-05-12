8fbb64a4fb1319453d0948a69d09f8c533effdeb
magdgar <magdgar@gmail.com>
2015-05-12 23:17:24 +0200

    nowa klasa - Hero:)

--- objects/pacman.py
+++ objects/pacman.py
@@ -1,23 +1,15 @@
 import media.sprites
-from media.dirtyrect import *
-from game_engine.gameengine import *
+from objects.hero import Hero
 
 
-class PacMan:
+class PacMan(Hero):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.speed = 4
+        super().__init__(x, y)
         self.animations = media.sprites.PacManAnim
-        self.direction = K_RIGHT
-        self.new_direction = K_RIGHT
-        self.movements = {K_UP: (0, -self.speed), K_RIGHT: (self.speed, 0),
-                          K_DOWN: (0, self.speed), K_LEFT: (-self.speed, 0), K_PAUSE: (0, 0)}
-
         for key, animation in self.animations.items():
             animation.play()
 
     def move(self, d_move):
-        add_dirty_rect(Rect(self.x - self.speed, self.y - self.speed, 40, 40))
+        super().move()
         self.x += d_move[0]
         self.y += d_move[1]
