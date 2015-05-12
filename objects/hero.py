788c27cbbaa489b71d10a243c1233dd11ee4b3dd
magdgar <magdgar@gmail.com>
2015-05-12 23:15:55 +0200

    nowa klasa - Hero:)

---
+++ objects/hero.py
@@ -0,0 +1,16 @@
+from game_engine.game_engine import *
+from media.dirtyrect import *
+
+
+class Hero:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 4
+        self.direction = K_RIGHT
+        self.new_direction = K_RIGHT
+        self.movements = {K_UP: (0, -self.speed), K_RIGHT: (self.speed, 0),
+                          K_DOWN: (0, self.speed), K_LEFT: (-self.speed, 0), K_PAUSE: (0, 0)}
+
+    def move(self):
+        add_dirty_rect(Rect(self.x - self.speed, self.y - self.speed, 40, 40))
