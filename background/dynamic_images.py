7df3edfd22c77899c521974732360f0d17d083a8
magdgar <magdgar@gmail.com>
2015-06-05 14:05:41 +0200

    refactor

--- background/dynamic_images.py
+++ background/dynamic_images.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.rect import Rect
 from background.background import repaint_fragment_of_background
-
+from media.constans import NUMBERS_DICT
 class DynamicImages:
 
     def __init__(self, window_surface):
@@ -13,7 +13,12 @@ class DynamicImages:
         pygame.Surface.blit(self.window_surface, pygame.image.load(self.live), (70, 580))
         pygame.Surface.blit(self.window_surface, pygame.image.load(self.live), (40, 580))
 
+        self.paint_numbers()
+
     def erase_live(self):
         self.lives -= 1
         repaint_fragment_of_background(self.window_surface, Rect(40 + self.lives*30, 580, 30, 30), False)
 
+    def paint_numbers(self):
+        for i in range(0, 10):
+            pygame.Surface.blit(self.window_surface, pygame.image.load(NUMBERS_DICT[i]), (260 + i*30, 580))
--- media/constans.py
+++ media/constans.py
@@ -12,5 +12,15 @@ ACTUAL_LVL = LEVEL_MAP.get("CLASSIC")
 ACTUAL_SCREENSCHOT = LEVEL_SCREENSCHOT.get("CLASSIC")
 ACTUAL_DOT_SCREENSCHOT = LEVEL_DOT_SCREENSCHOT.get("CLASSIC")
 
-
-
+IMG_0 = 'resources/numbers/0.png'
+IMG_1 = 'resources/numbers/1.png'
+IMG_2 = 'resources/numbers/2.png'
+IMG_3 = 'resources/numbers/3.png'
+IMG_4 = 'resources/numbers/4.png'
+IMG_5 = 'resources/numbers/5.png'
+IMG_6 = 'resources/numbers/6.png'
+IMG_7 = 'resources/numbers/7.png'
+IMG_8 = 'resources/numbers/8.png'
+IMG_9 = 'resources/numbers/9.png'
+# dictionary used for painting
+NUMBERS_DICT = {0: IMG_0, 1: IMG_1, 2: IMG_2, 3: IMG_3, 4: IMG_4, 5: IMG_5, 6: IMG_6, 7: IMG_7, 8: IMG_8, 9: IMG_9}
